3bb2445996e41e5931b51042bb10e82258b5d713
gopivasala01 <gopi.v@beetlerim.com>
2023-02-01 00:48:20 -0500

    02/01/2023 run

--- src/main/java/mainPackage/RunnerClass.java
+++ src/main/java/mainPackage/RunnerClass.java
@@ -71,7 +71,7 @@ public class RunnerClass
 		
 		if(getBuildings==true)
 		{
-			saveButtonOnAndOff = false;
+			saveButtonOnAndOff = true;
 			try
 			{
 			for(int i=0;i<pendingBuildingList.length;i++)
