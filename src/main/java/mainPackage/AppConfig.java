340d8560ec8bbd732295f48a4826ba6d06e8f1b4
gopivasala01 <gopi.v@beetlerim.com>
2023-02-28 03:52:18 -0500

    Scheduling Build

--- src/main/java/mainPackage/AppConfig.java
+++ src/main/java/mainPackage/AppConfig.java
@@ -7,7 +7,7 @@ public class AppConfig
    public static String password ="HomeRiver1#";
    
    public static String quertyToFetchPendingBuildingsListFromETLSource = "Select Company,[Building/Unit Abbreviation],TargetRent,TargetDeposit from automation.TargetRent where Status = 'Pending'"; 
-   public static String failedBuildingsList = "Select Company,[Building/Unit Abbreviation],TargetRent,TargetDeposit from automation.TargetRent where Notes in ('Target Deposit is not updated','Unable to update details','Error logging into PW','Error opening browser','Building Not Found','Issue in selecting Building','Target Rent is not updated','')";
+   public static String failedBuildingsList = "Select Company,[Building/Unit Abbreviation],TargetRent,TargetDeposit from automation.TargetRent where Notes in ('Target Deposit is not updated','Unable to update details','Error logging into PW','Error opening browser','Building Not Found','Issue in selecting Building','Target Rent is not updated','') and FoRMAT(completedOn,'MM-dd-yyyy')=FoRMAT(getdate(),'MM-dd-yyyy')";
    public static String connectionUrl = "jdbc:sqlserver://azrsrv001.database.windows.net;databaseName=HomeRiverDB;user=service_sql02;password=xzqcoK7T";
 
    public static String reasonForChange = "HRG - Automation";
@@ -20,7 +20,7 @@ public class AppConfig
    public static String fromEmail = "bireports@beetlerim.com";
    public static String fromEmailPassword = "Welcome@123";
    
-   public static String toEmail =  "gopi.v@beetlerim.com";  //"msanfilippo@homeriver.com,amesa@homeriver.com,kespinoza@homeriver.com,mjackson@homeriver.com";
+   public static String toEmail =  "amesa@homeriver.com,kespinoza@homeriver.com,mjackson@homeriver.com";
    public static String CCEmail = "gopi.v@beetlerim.com";
    
    public static String mailSubject = "Target Rent Update for  ";
